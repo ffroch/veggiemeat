6b7a48df7f3e90ba12f52d386b92f6742e38f6b9
Franz-Ferdinand Roch <franz-ferdinand.roch@vetmeduni.ac.at>
2024-03-04 14:16:20 +0100

    Refreshing all data before publishing

--- A_MakeDataStructure.R
+++
@@ -1,6 +0,0 @@
-###file preparations----
-#create outputdir
-dir.create("04-finalRplots")
-dir.create("05-analysisRplots")
-dir.create("06-outputfiles")
-dir.create("06-outputfiles/trimmedSangerfasta")
